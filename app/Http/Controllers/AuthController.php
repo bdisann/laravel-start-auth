5d1ca34b2e99311e64135907e1ee8883d412e3ba
Budi Santoso <budi@jkt1.ebdesk.com>
2021-11-27 11:35:13 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthRequests\LoginRequest;
+use App\Http\Requests\AuthRequests\LogoutRequest;
+
+// use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $req){
+        
+    }
+
+    public function logout(LogoutRequest $req){
+
+    }
+}
